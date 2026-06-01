e608137a7f485546c71a2c4eb3f8e8ed78a02514
DonQuijoteDCLXVI <donquijotedclxvi@gmail.com>
2026-06-02 00:45:27 +0500

    New commit with updated bindings
    Added one line to hyprshot bindings

---
+++ hypr/bindings.lua
@@ -0,0 +1,30 @@
+local mainMod = "SUPER"
+
+-- Main key bindings
+hl.bind(mainMod .. " + Return", hl.dsp.exec_cmd("alacritty"))
+hl.bind(mainMod .. " + H", hl.dsp.exec_cmd("helium-browser"))
+hl.bind(mainMod .. " + P", hl.dsp.exec_cmd("wofi --show drun"))
+hl.bind(mainMod .. " + T", hl.dsp.exec_cmd("sudo throne-bin"))
+local closeWindowBind = hl.bind(mainMod .. " + C", hl.dsp.window.close())
+
+-- Screenshot via hyprshot utility
+
+hl.bind(mainMod .. " + PRINT", hl.dsp.exec_cmd("hyprshot -m window"))
+hl.bind("PRINT", hl.dsp.exec_cmd("hyprshot -m output"))
+hl.bind(mainMod .. " + SHIFT + S", hl.dsp.exec_cmd("hyprshot -m region"))
+hl.bind(mianMod .. " + ALT + S", hl.dsp.exec_cmd("hyprshot -m region --clipboard-only"))
+
+-- Laptop multimedia keys for volume and LCD brightness
+hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"), { repeating = true })
+hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"), { repeating = true })
+hl.bind("XF86AudioMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"))
+hl.bind("XF86AudioMicMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"))
+hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl s 5%+"), { repeating = true })
+hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl s 5%-"), { repeating = true })
+
+-- Switch workspaces
+for i = 1, 10 do
+	local key = i % 10
+	hl.bind(mainMod .. " + " .. key,	hl.dsp.focus({workspace = i}))
+	hl.bind(mainMod .. " + SHIFT +" .. key, hl.dsp.window.move({workspace = i}))
+end
